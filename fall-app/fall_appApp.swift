f6d2e6a79a718b3e018887303ada8a4f157d2392
Seung-Gu Lee <awesomeguy4k@gmail.com>
2023-04-06 16:15:25 -0400

    Initial Commit

---
+++ fall-app/fall_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  fall_appApp.swift
+//  fall-app
+//
+//  Created by Seung-Gu Lee on 4/6/23.
+//
+
+import SwiftUI
+
+@main
+struct fall_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
